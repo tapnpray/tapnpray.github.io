e2eb69b1475e555b59c440c4ece2938adf9e23dd
Robert Kraft <tapnpray@gmail.com>
2026-04-06 17:32:26 -0400

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "tapnpray-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/logo.svg"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
